d5574169befbc2a82a371eff80c0571088233595
GEONWOOKIM <andrew00829@gmail.com>
2025-03-12 01:23:27 +0900

    sdfs

--- newfile.c
+++ newfile.c
@@ -1,2 +1,16 @@
-김건우
-pull test 2
+
+#include <stdio.h>
+
+int main(void)
+{
+    int x, y;
+
+    x=3;
+    y=4;
+
+    printf("x=%d\n", x);
+    printf("y=%d\n", y);
+    printf("x+y=%d\n", x+y);
+
+    return 0;
+}
