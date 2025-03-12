d4ecddc02e1e3cdce3c8e7fdd2e0a50890571dad
GEONWOOKIM <andrew00829@gmail.com>
2025-03-12 23:13:57 +0900

    1주차 수정본

--- exam3.c
+++ exam3.c
@@ -7,7 +7,7 @@ int main(void)
     printf("정수를 입력하세요 : ");
     scanf("%d %d", &x, &y);
 
-    printf("입력값 : %d %d\n", &x, &y);
+    printf("입력값 : %d %d\n", x, y);
 
     printf("몫 : %d\n", x/y);
     printf("나머지 : %d\n", x%y);
